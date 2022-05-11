0cdb19095cb5c1f93d66af0a532755fac47a34ac
Bouhdima <bouhdimabster@gmail.com>
2022-05-11 10:27:55 +0400

    second commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-
+import data from './data';
 function App() {
   return (
     <div className="App">
@@ -7,7 +7,23 @@ function App() {
         <a href="/">Bouhdima</a>
       </header>
       <main>
-        <h1>list of product</h1>
+        <h1>Featured product</h1>
+        <div className="products">
+          {data.products.map((product) => (
+            <div className="product" key={product.slug}>
+              <a href={`/products/${product.slug}`}>
+                <img src={product.image} alt={product.name} />
+              </a>
+              <div className="product-info">
+                <a href={`/products/${product.slug}`}>
+                  <p>{product.name}</p>
+                </a>
+                <p><strong>${product.price}</strong></p>
+              </div>
+              <button className="btn btn-warning">Add to cart</button>
+            </div>
+          ))}{' '}
+        </div>
       </main>
     </div>
   );
