6d00d9b8a0f54e1a00a7ecc889a41343b3aa6446
Bouhdima <bouhdimabster@gmail.com>
2022-05-11 08:37:13 +0400

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <a href="/">Bouhdima</a>
+      </header>
+      <main>
+        <h1>list of product</h1>
+      </main>
+    </div>
+  );
+}
+
+export default App;
